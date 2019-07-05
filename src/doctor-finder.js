3f0a3867c32eadca098d7d7575c8f77102812be4
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-05 12:02:32 -0700

    research postman to get my API to work.

--- src/doctor-finder.js
+++ src/doctor-finder.js
@@ -3,7 +3,7 @@ export class DoctorFinder {
   getDoctorsInfo(symptom) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `ADD API HERE`;
+      let url = `ADD API HERE ---- `;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.responce);
