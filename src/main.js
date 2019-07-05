72ba132e511d8f5b98eb1e0a3c4b58a05856dc06
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-05 09:32:51 -0700

    add imports and promises to my back end logic.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
-import { doctors } from './sample-class.js';
+import { Doctor } from '.sample-class';
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-$(document).ready(function() {
+$(document).ready(function () {
+  $('.find').click(function () {
+    let doctor = $('#doctor').val();
+    $('#doctor').val('');
+    let doctorServices = new Doctor();
+    let promise = doctorServices.getDoctorsInfo(doctor);
+    promise.then(function (response) {
+      let body = JSON.parse(response);
+      $('.showDoctor').text(body.)
+    }
+  }
 
 });
--- src/sample-class.js
+++ src/sample-class.js
@@ -3,6 +3,16 @@ export class Doctor {
   getDoctorsInfo(symptom) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = 
-    }
+      let url = ``
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.responce);
+        } else {
+          refect(Error(request.statusText));
+        }
+      };
+
+      request.open('GET', url, true);
+      request.send();
+    });
   }
