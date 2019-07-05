5770b513e84db61918f25c88bae9afea55becee1
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-05 08:29:06 -0700

    Create a new API and set up my initial files and webpack.

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { Sample } from './sample-class.js';
+import './styles.css';
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+$(document).ready(function() {
+
+});
---
+++ src/sample-class.js
@@ -0,0 +1,11 @@
+export class Sample {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+
+  checkType() {
+    //Function body goes here.
+  }
+}
