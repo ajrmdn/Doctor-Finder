fcd55907e98fbe37530a9c6d8a3819d453b9230d
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-05 13:30:49 -0700

    add a working API to my back end logic.

--- src/doctor-finder.js
+++ src/doctor-finder.js
@@ -3,12 +3,12 @@ export class DoctorFinder {
   getDoctorsInfo(symptom) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `ADD API HERE ---- `;
+      let url = `https://betterdoctor.com/?q=portland,oregon&appid=06c61ef45efcb6781b9987e413432c51`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.responce);
         } else {
-          refect(Error(request.statusText));
+          reject(Error(request.statusText));
         }
       };
 
