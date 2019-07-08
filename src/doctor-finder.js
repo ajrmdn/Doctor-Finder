fc2fe21483d8abf5dad1632911a58dac6bb3a980
Abdellah Ramadan <ajrmdn@gmail.com>
2019-07-07 17:25:07 -0700

    add .env file

--- src/doctor-finder.js
+++ src/doctor-finder.js
@@ -1,10 +1,9 @@
-// import { apiKey } from './../.env';
 export class DoctorFinder {
 
-  getDoctorsInfo(word) {
+  getDoctorsInfo(word, condition) {
     return new Promise(function (resolve, reject) {
       let request = new XMLHttpRequest();
-      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${word}&location=45.151%2C-122.413%2C100&user_location=45.151%2C-122.413&user_key=06c61ef45efcb6781b9987e413432c51`;
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?query=${word}&location=45.151%2C-122.413%2C100&user_location=45.151%2C-122.413&user_key=${process.env.exports.apiKey}`;
       request.onload = function () {
         if (this.status === 200) {
           resolve(request.response);
--- src/main.js
+++ src/main.js
@@ -8,11 +8,17 @@ $(document).ready(function () {
   $('.find').click(function () {
     let doctor = $('#doctor').val();
     $('#doctor').val('');
+
     let doctorServices = new DoctorFinder();
     let promise = doctorServices.getDoctorsInfo(doctor);
+
     promise.then(function (response) {
       let body = JSON.parse(response);
-      if (body.data.length > 0) {
+
+      if (body.data.length === 0) {
+        $('.showDoctor').append('No doctors in your area');
+      }
+
         for (var i = 0; i < body.data.length; i++) {
           let firstName = body.data[i].profile.first_name;
           let lastName = body.data[i].profile.last_name;
@@ -22,16 +28,15 @@ $(document).ready(function () {
           let newPatients = body.data[i].practices[0].accepts_new_patients;
 
           let doctor = new DoctorFinder(firstName, lastName, address, phone, website, newPatients);
-          if (body.data[i].length < 1) {
-            return 'No doctors in your area';
-          }
 
           $('.showDoctor').append(`${firstName} ${lastName}`);
           $('.showDoctor').append(`<ul><li>${address}</li><li>${phone}</li><li>Website: ${website}</li><li>Taking new patient: ${newPatients}</li></ul>`);
-
-
         }
-      }
-    });
+
+      });
+       // function (error) {
+       //       $('.showDoctor').text(`There is an error in your request: ${error.message}`);
+       //     };
+
   });
 });
